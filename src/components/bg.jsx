37b7042fcaeb9743520a9ae6d56698773f281cdf
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2025-04-18 19:04:14 +0530

    features page

--- src/components/bg.jsx
+++ src/components/bg.jsx
@@ -1,6 +1,8 @@
 const FancyBg = () => {
     return (
-        <div class="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_0.7px,transparent_0.5px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+        <div className="fixed inset-0 -z-10 w-full min-h-screen bg-white bg-[linear-gradient(to_right,#f0f0f0_0.7px,transparent_0.5px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+            <div className="bg-[radial-gradient(circle_500px_at_100%_200px,#d5c5ff,transparent)]"></div>
+        </div>
     );
 };
 
